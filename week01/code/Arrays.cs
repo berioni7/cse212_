6c7fcfb7744ceecbd856519d2fcd1bd4e7133e4a
berioni7 <gberioni@byupathway.edu>
2026-01-12 20:15:16 -0300

    Arrays.cs

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,14 +6,19 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
+    public static double[] MultiplesOf(double number, int length) // number : base number - length : how many multiples
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+            // step 1: create a space in memory that stores data. In this case, it will be an array. 
 
-        return []; // replace this return statement with your own
+            double[] dataEnter = new double[length]; // I created an array named dataEnter that can hold 'length' number of doubles. 
+            // Thinking like “Reserve exactly X spaces in memory now.” Length is my size.
+
+            for (int i = 0; i < length; i++) // here I create a for loop that will iterate until i is less than length
+            {
+                dataEnter[i] = number * (i + 1); // for each iteration, I am setting the value of the array at index i to be equal to 
+                // number multiplied by (i + 1) 
+            }
+            return dataEnter; // finally, I return the array dataEnter
     }
 
     /// <summary>
@@ -25,9 +30,14 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        for (int i = 0; i < amount; i++)
+        {
+            int lastElementIndex = data.Count - 1;  // get the index of the last element in the list
+            int valueToMove = data[lastElementIndex]; // get the value of the last element in the list
+
+            data.RemoveAt(lastElementIndex); // remove the last element from the list
+
+            data.Insert(0, valueToMove); // insert the value we removed at the front of the list
+        }         
     }
 }
